d0f159dcf1037429a338cfa802614dee8252dcdb
Bharath Chandra <bharathchandra.chandra96@gmail.com>
2018-12-21 00:53:02 -0800

    Update background.js

--- background.js
+++ background.js
@@ -16,20 +16,16 @@ chrome.runtime.onInstalled.addListener(function(){
 });
 
 function assignWorker(){
-  console.log('Creating Worker');
   worker = new Worker('worker.js');
   worker.addEventListener('message', function(e){
       var data = e.data;
       switch(data.cmd){
         case 'timespent':
-          console.log('-------------SETTING THE VALUES------------------');
-          console.log(data['website'],data['timespent']);
           setData(data['website'], data['timespent']);
           break;
         case 'close':
-          
-      }
-    
+           
+      } 
   });
 }
 
@@ -44,11 +40,8 @@ function search(key){
 
 //setting up the keys in database.
 chrome.storage.local.get(null, function(result){
-    console.log(result);
     keys_in_db = Object.keys(result);
     values_in_db = Object.values(result);
-    console.log(keys_in_db);
-    console.log(values_in_db);
 });
 
 function setData(key, value){
@@ -57,19 +50,13 @@ function setData(key, value){
   key = key.toString();
   //dataObj[key] = value;
   chrome.storage.local.get(null, function(result){
-    console.log('----get in set----');
     if(result[key]){
       //alert(result[key]);
       value += parseInt(result[key]);
-      console.log('the value');
       result[key] = value;
-      console.log(result[key]);
       checkTimer(key, result[key]);
       var time_limit = s;
-      console.log('The time_limit: ');
-      console.log(time_limit);
       if(time_limit>0 && value>time_limit){
-        console.log('the drop is called');
         dropTab();
       }
     }
@@ -77,7 +64,6 @@ function setData(key, value){
       result[key]=parseInt(value);
     }
     chrome.storage.local.set(result, function() {
-      console.log('The key-value: '+key+' '+result[key].toString());
     });
 });
   }
@@ -97,11 +83,9 @@ function checkTimer(key, time){
   
   chrome.storage.local.get('timer', function(result){
     if(key+'_timer' in result['timer']){
-      console.log(key+' timer is '+result['timer'][key+'_timer'].toString());
       s =  parseInt(result['timer'][key+'_timer']);
     }
     else{
-      console.log('no key timer');
       s = -1;
     }
     
@@ -110,7 +94,6 @@ function checkTimer(key, time){
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){  
 
-  console.log('called UPDATED');
 
   if(changeInfo.status === "complete"){
     var url = tab.url;
@@ -138,12 +121,6 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
     sendMessage(e);
     setStartTime(url, tabId);
     }
-    console.log("Start Time");
-    console.log(startTimes);
-    console.log("Mapper");
-    console.log(mapper);
-    console.log("Total Time");
-    console.log(totalTime);
   }}
     else{
       console.log('No url to show');
@@ -152,7 +129,6 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
 
 chrome.tabs.onRemoved.addListener(function(tabId, isWindowClosing){
 
-  console.log('called ONREMOVED');
     
     if(tabId in mapper){
       var url = mapper[tabId];
@@ -162,19 +138,12 @@ chrome.tabs.onRemoved.addListener(function(tabId, isWindowClosing){
       delete mapper[tabId];
       delete startTimes[url+'_'+tabId.toString()];
      
-      console.log("Start Time");
-      console.log(startTimes);
-      console.log("Mapper");
-      console.log(mapper);
-      console.log("Total Time");
-      console.log(totalTime);
     }
 
 });
 
 // Is called when the active tab is changed.
 chrome.tabs.onActivated.addListener(function(activeInfo){
-  console.log('called ONACTIVATED');
 
   var tabId = activeInfo['tabId'];
   if(currentTab in mapper)
@@ -182,12 +151,6 @@ chrome.tabs.onActivated.addListener(function(activeInfo){
   if(tabId in mapper)
     setStartTime(mapper[tabId],tabId);
   setCurrentTab(tabId);
-  console.log("Start Time");
-  console.log(startTimes);
-  console.log("Mapper");
-  console.log(mapper);
-  console.log("Total Time");
-  console.log(totalTime);
 });
 
 // Set the startaTimes of given url and tabId.
@@ -203,12 +166,9 @@ function setCurrentTab(tabId){
     currentTab = tabId;
     if(currentTab in mapper)
       sendMessage();
-    console.log(mapper[currentTab]);
-    console.log("current tab");
 }
 
 function sendMessage(e){
-  console.log('Sending message to Worker');
   worker.postMessage({'cmd':'start','website':mapper[currentTab],'timer':e});
 }
 
